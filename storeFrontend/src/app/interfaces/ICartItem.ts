df879da38e843a4d5cf429c6ed2a548e2568e0a1
Addie Nelson <Adella.L.Nelson@gmail.com>
2022-08-10 16:38:45 -0500

    still working on shopping cart/cart item list

--- storeFrontend/src/app/interfaces/ICartItem.ts
+++ storeFrontend/src/app/interfaces/ICartItem.ts
@@ -2,5 +2,6 @@ import {IProduct} from "./IProduct";
 
 export interface ICartItem extends IProduct{
   //todo use this to tie to quantity counter in shopping cart
+  price: number;
   count: number;
 }
